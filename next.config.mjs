ddf006b5ea6c82b7c96ac66734bba302555e97f0
Prince <89233327+princemerja1@users.noreply.github.com>
2024-02-24 23:55:15 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,12 @@
 const nextConfig = {};
 
 export default nextConfig;
+export default {
+  // Other configurations...
+  exportPathMap: async function () {
+    return {
+      '/': { page: '/' },
+      // Add other pages as needed
+    };
+  },
+};
